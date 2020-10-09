c4a6424ac3113200adca1324471be5aa048b524d
Deepanjan1498 <deepanjansharma43@gmail.com>
2020-10-09 17:12:52 +0530

    [Deepanjan]Add . uc3 is solved to print the board

--- src/capgemini/tictactoegame/TicTacToeGame.java
+++ src/capgemini/tictactoegame/TicTacToeGame.java
@@ -4,8 +4,10 @@ import java.util.Scanner;
 
 public class TicTacToeGame {
 	public static void main(String[] args) {
-		createBoard();
+		char[] board = createBoard();
 		selectLetter();
+		showBoard(board);
+
 	}
 
 	/**
@@ -32,6 +34,20 @@ public class TicTacToeGame {
 			System.out.println("Player has choosen " + choice + " and Computer has to choose X");
 		else
 			System.out.println("Player has choosen " + choice + " and Computer has to choose O");
+
+	}
+
+	/**
+	 * uc3
+	 */
+	public static void showBoard(char[] board) {
+		System.out.println("/---|---|---\\");
+		System.out.println("| " + board[0] + " | " + board[1] + " | " + board[2] + " |");
+		System.out.println("|-----------|");
+		System.out.println("| " + board[3] + " | " + board[4] + " | " + board[5] + " |");
+		System.out.println("|-----------|");
+		System.out.println("| " + board[6] + " | " + board[7] + " | " + board[8] + " |");
+		System.out.println("/---|---|---\\");
 
 	}
 
